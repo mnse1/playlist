184202445b63de2218588e73a5c064389377d491
MNSE <alstj1030@gmail.com>
2025-05-02 16:26:36 +0900

    1

--- main.py
+++ main.py
@@ -16,9 +16,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["https://playlist-frontend1.s3-website.ap-northeast-2.amazonaws.com"],
+    allow_origins=["https://playlist-frontend1.s3-website.ap-northeast-2.amazonaws.com"],
     allow_origins=["*"],
-    # allow_credentials=True,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
